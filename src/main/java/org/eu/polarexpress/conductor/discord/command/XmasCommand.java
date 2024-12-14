648b349ab0229275978df48e06ff025b0855a1cf
Schnitzel5 <tangphilip3@gmail.com>
2024-12-14 18:02:40 +0100

    small fix

--- src/main/java/org/eu/polarexpress/conductor/discord/command/XmasCommand.java
+++ src/main/java/org/eu/polarexpress/conductor/discord/command/XmasCommand.java
@@ -20,24 +20,28 @@ public class XmasCommand implements SlashCommand {
                 .map(ApplicationCommandInteractionOptionValue::asBoolean)
                 .orElse(false);
         var roleId = event.getInteraction().getGuild().map(guild ->
-                guild.getRoles().filter(role -> role.getName().equalsIgnoreCase("xmasarrived"))
-                        .blockFirst())
+                        guild.getRoles().filter(role -> role.getName().equalsIgnoreCase("xmasarrived"))
+                                .blockFirst())
                 .block();
         if (roleId == null) {
             return event.deferReply().then(event.createFollowup().withContent("Role 'xmasarrived' not found!"));
         }
-        event.getInteraction().getGuild().map(guild ->
-                guild.getMembers()
-                        .publishOn(Schedulers.boundedElastic())
-                        .filter(member -> !member.isBot())
-                        .flatMap(member -> {
-                            if (xmas) {
-                                member.addRole(roleId.getId()).block();
-                            } else {
-                                member.removeRole(roleId.getId()).block();
-                            }
-                            return Mono.empty();
-                        })).subscribe();
+        var guild = event.getInteraction().getGuild().block();
+        if (guild == null) {
+            return event.deferReply().then(event.createFollowup().withContent("Error while fetching guild!"));
+        }
+        guild.getMembers()
+                .filter(member -> !member.isBot())
+                .publishOn(Schedulers.boundedElastic())
+                .flatMap(member -> {
+                    if (xmas) {
+                        member.addRole(roleId.getId()).block();
+                    } else {
+                        member.removeRole(roleId.getId()).block();
+                    }
+                    return Mono.empty();
+                })
+                .subscribe();
         return event.deferReply().then(event.createFollowup().withContent(
                 (xmas ? "Added" : "Removed") + " xmas role " + (xmas ? "to" : "from") + " everyone!")
         );
